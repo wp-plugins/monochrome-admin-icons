3c82d98102b28005fd9a93b112c87bbf0feeead7
pdclark <pdclark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-22 02:52:47 +0000

    Tagging version trunk
    git-svn-id: https://plugins.svn.wordpress.org/monochrome-admin-icons/tags/trunk@562128 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- monochrome-admin-icons.php
+++ monochrome-admin-icons.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Monochrome Admin Icons
 Description: Make plugin admin icons follow <a href="http://dotorgstyleguide.wordpress.com/outline/icons/">WordPress guidelines</a>: Monochrome by default, color when active.
-Version: 1.1.1
+Version: 1.1.2
 Author: Brainstorm Media
 Author URI: http://brainstormmedia.com
 */
@@ -11,31 +11,33 @@ add_action('admin_init', create_function('', 'new Storm_Monochrome_Admin_Icons()
 
 class Storm_Monochrome_Admin_Icons {
 
-	var $version = '1.1.1';
+	var $version = '1.1.2';
 
 	/**
-	 * CSS filters supported by IE7+, Firefox 3.5+, Chrome 18, and Safari 6+
+	 * CSS/SVG filters supported by IE7+, Firefox 3.5+, Chrome 18+, and Safari 6+
 	 * @see http://stackoverflow.com/questions/609273/convert-an-image-to-grayscale-in-html-css
 	 * 
-	 * Javascript + HTML5 Canvas filters as fallback for Chrome < 18 and Safari < 6
+	 * Javascript + HTML5 Canvas filters as fallback for Chrome 17 and Safari 5
 	 * @see http://webdesignerwall.com/tutorials/html5-grayscale-image-hover
 	 */
 	function __construct(){
-		wp_enqueue_script('monochrome-admin-icons', plugin_dir_url( __FILE__ ).'monochrome.js', array('jquery'), $this->version, false);
+		if ( WP_DEBUG || BSM_DEVELOPMENT ) { $this->version .= '-dev-'.time(); }
 
-		add_action('admin_head', array($this, 'filter_css') );
-		add_action('admin_footer', array($this, 'filter_svg') );
+		add_action( 'admin_head',   array($this, 'filter_css') );
+		add_action( 'admin_footer', array($this, 'filter_svg') );
+
+		wp_enqueue_script('monochrome-admin-icons', plugin_dir_url( __FILE__ ).'monochrome.js', array('jquery'), $this->version, false);
 	}
 
 	function filter_css() {
-		?><style media="screen">
-			#adminmenu > li.wp-not-current-submenu img { filter: url(#grayscale); ?>); /* Firefox 3.5+ */ filter: gray; /* IE6-9 */ -webkit-filter: grayscale(1); /* Google Chrome & Webkit Nightlies */ }
+		?><style>
+			#adminmenu > li.wp-not-current-submenu img { filter: url(#grayscale); ?>); /* Firefox 3.5+ */ filter: gray; /* IE6-9 */ -webkit-filter: grayscale(1); /* Google Chrome & Webkit Nightlies */ -moz-opacity:.8; filter:alpha(opacity=80); opacity:.80;}
 			#adminmenu > li.wp-not-current-submenu:hover img { filter: none; -webkit-filter: grayscale(0); }
 		</style><?php
 	}
 
 	/**
-	 * SVG to support the FireFox filter. 
+	 * SVG filter for Firefox 3.5+. 
 	 * Inserting it inline avoids servers not configured to send correct SVG headers
 	 */
 	function filter_svg(){
